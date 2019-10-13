a55f71c0dde5c1ea952156b27f473cc8e61a290a
Kyle Rubenok <kyle@rubenok.ca>
2019-10-12 21:37:00 -0700

    Connected to MongoDB Atlas

--- src/points.js
+++ src/points.js
@@ -1,72 +1,103 @@
-const axios = require('axios');
-const debug = require('debug')('slash-command-template:ticket');
-const qs = require('querystring');
-const users = require('./users');
+const axios = require("axios");
+const debug = require("debug")("slash-command-template:ticket");
+const qs = require("querystring");
+const users = require("./users");
+const MongoClient = require("mongodb").MongoClient;
+const uri = process.env.MONGODB_URL;
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
 /*
  *  Send ticket creation confirmation via
  *  chat.postMessage to the user who created it
  */
-const sendConfirmation = (points) => {
-  axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
-    token: process.env.SLACK_ACCESS_TOKEN,
-    channel: process.env.SLACK_POINTS_CHANNEL,
-    // as_user: false,
-    text: 'Points submitted!',
-    attachments: JSON.stringify([
-      {
-        title: `Points submitted by ${points.userEmail}`,
-        // Get this from the 3rd party helpdesk system
-        // TODO: need to get the URL for the google sheet in here somehow
-        // title_link: 'http://example.com',
-        text: points.text,
-        fields: [
-          {
-            title: 'Team Name',
-            value: points.team,
-            short: true,
-          },
-          {
-            title: 'Number',
-            value: points.number,
-          },
-          {
-            title: 'Description',
-            value: points.description || 'None provided',
-          },
-        ],
-      },
-    ]),
-  })).then((result) => {
-    debug('sendConfirmation: %o', result.data);
-  }).catch((err) => {
-    debug('sendConfirmation error: %o', err);
-    console.error(err);
-  });
+const sendConfirmation = points => {
+    client.connect(err => {
+        const collection = client
+            .db(process.env.EVENT_NAME)
+            .collection("BonusPoints");
+        var pointObject = {
+            team: points.team,
+            points: points.number,
+            description: points.description,
+            committee: points.userEmail
+        };
+        collection.insertOne(pointObject, function(err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+        });
+        client.close();
+    });
+    axios
+        .post(
+            "https://slack.com/api/chat.postMessage",
+            qs.stringify({
+                token: process.env.SLACK_ACCESS_TOKEN,
+                channel: process.env.SLACK_POINTS_CHANNEL,
+                // as_user: false,
+                text: "Points submitted!",
+                attachments: JSON.stringify([
+                    {
+                        title: `Points submitted by ${points.userEmail}`,
+                        text: points.text,
+                        fields: [
+                            {
+                                title: "Team Name",
+                                value: points.team,
+                                short: true
+                            },
+                            {
+                                title: "Number",
+                                value: points.number
+                            },
+                            {
+                                title: "Description",
+                                value: points.description || "None provided"
+                            }
+                        ]
+                    }
+                ])
+            })
+        )
+        .then(result => {
+            debug("sendConfirmation: %o", result.data);
+        })
+        .catch(err => {
+            debug("sendConfirmation error: %o", err);
+            console.error(err);
+        });
 };
 
 // Create helpdesk ticket. Call users.find to get the user's email address
 // from their user ID
 const create = (userId, submission) => {
-  const points = {};
+    const points = {};
 
-  const fetchUserEmail = new Promise((resolve, reject) => {
-    users.find(userId).then((result) => {
-      debug(`Find user: ${userId}`);
-      resolve(result.data.user.profile.email);
-    }).catch((err) => { reject(err); });
-  });
+    const fetchUserEmail = new Promise((resolve, reject) => {
+        users
+            .find(userId)
+            .then(result => {
+                debug(`Find user: ${userId}`);
+                resolve(result.data.user.profile.email);
+            })
+            .catch(err => {
+                reject(err);
+            });
+    });
 
-  fetchUserEmail.then((result) => {
-    points.userId = userId;
-    points.userEmail = result;
-    points.number = submission.number;
-    points.description = submission.description;
-    points.team = submission.team;
-    sendConfirmation(points);
+    fetchUserEmail
+        .then(result => {
+            points.userId = userId;
+            points.userEmail = result;
+            points.number = submission.number;
+            points.description = submission.description;
+            points.team = submission.team;
+            sendConfirmation(points);
 
-    return points;
-  }).catch((err) => { console.error(err); });
+            return points;
+        })
+        .catch(err => {
+            console.error(err);
+        });
 };
 
 module.exports = { create, sendConfirmation };
